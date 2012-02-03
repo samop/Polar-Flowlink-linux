efc9e0979f53f38fe6cab5bab29ccecd8ee76c57
Samo Penic <samo.penic@gmail.com>
2012-02-03 20:33:14 +0100

    Fixed a bug. Individual trainings were not ordered properly.

--- web/get_ctraining_duration.php
+++ web/get_ctraining_duration.php
@@ -12,11 +12,14 @@ if($db->dbhandle===NULL){
 //BUG previous week does not work if current week is 1!
 //$a=$_GET['previous'];
 
+//individual training;
 if($a==0){
-$query="select to_char(start_time,'DD.MM HH24:MI Dy') as start_time,avg_hr, max_hr,calories,extract(hours from time_in_zone_1*60) as z1,extract(hours from time_in_zone_2*60) as z2, extract(hours from time_in_zone_3*60) as z3, extract(hours from (duration-time_in_zone_1-time_in_zone_2-time_in_zone_3)*60) as nozone, time_in_zone_1, time_in_zone_2, time_in_zone_3,duration-time_in_zone_1-time_in_zone_2-time_in_zone_3 as time_in_nozone  from training order by start_time;";
+$query="select start_time as order_time, to_char(start_time,'DD.MM HH24:MI Dy') as start_time,avg_hr, max_hr,calories,extract(hours from time_in_zone_1*60) as z1,extract(hours from time_in_zone_2*60) as z2, extract(hours from time_in_zone_3*60) as z3, extract(hours from (duration-time_in_zone_1-time_in_zone_2-time_in_zone_3)*60) as nozone, time_in_zone_1, time_in_zone_2, time_in_zone_3,duration-time_in_zone_1-time_in_zone_2-time_in_zone_3 as time_in_nozone  from training order by order_time;";
 } elseif($a==1) {
+//training/day
 $query="select to_char(date(start_time),'Dy DD.MM') as start_time, avg(avg_hr)::integer as avg_hr, max(max_hr) as max_hr ,sum(calories) as calories,sum(extract(hours from time_in_zone_1*60)) as z1,sum(extract(hours from time_in_zone_2*60)) as z2, sum(extract(hours from time_in_zone_3*60)) as z3, sum(extract(hours from (duration-time_in_zone_1-time_in_zone_2-time_in_zone_3)*60)) as nozone, sum(time_in_zone_1) as time_in_zone_1, sum(time_in_zone_2) as time_in_zone_2, sum(time_in_zone_3) as time_in_zone_3,sum(duration-time_in_zone_1-time_in_zone_2-time_in_zone_3) as time_in_nozone  from training group by date(start_time) order by date(start_time);";
 } else {
+//training/week
 $query="select extract(week from start_time) as start_time, avg(avg_hr)::integer as avg_hr, max(max_hr) as max_hr ,sum(calories) as calories,sum(extract(hours from time_in_zone_1*60)) as z1,sum(extract(hours from time_in_zone_2*60)) as z2, sum(extract(hours from time_in_zone_3*60)) as z3, sum(extract(hours from (duration-time_in_zone_1-time_in_zone_2-time_in_zone_3)*60)) as nozone, sum(time_in_zone_1) as time_in_zone_1, sum(time_in_zone_2) as time_in_zone_2, sum(time_in_zone_3) as time_in_zone_3,sum(duration-time_in_zone_1-time_in_zone_2-time_in_zone_3) as time_in_nozone  from training group by extract(week from start_time) order by extract(week from start_time);";
 }
 
